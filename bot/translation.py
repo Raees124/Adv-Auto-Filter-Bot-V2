99ee757598ca554256b79121369d86b22584991f
Raees124 <86163373+Raees124@users.noreply.github.com>
2021-06-21 11:45:53 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,8 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@KISWA_ONLINE_LIBRARY
+
 </i>"""    
     
     HELP_TEXT = """
@@ -43,12 +44,12 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@KISWA_ONLINE_LIBRARY
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/raeeskalanthode">@raeeskalanthode</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
